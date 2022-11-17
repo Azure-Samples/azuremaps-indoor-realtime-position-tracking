0400f8ff10227964bec280b26eeb35db85a8213a
Oscar Naim <62676340+onaim-msft@users.noreply.github.com>
2022-11-17 15:52:49 -0800

    Fixes to script

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -1,4 +1,4 @@
-echo "Post deployment script starting...\n"
+echo "Post deployment script starting..."
 
 iothubname=$1
 rgname=$2
@@ -7,11 +7,11 @@ funcappid=$4
 storagename=$5
 containername=$6
 
-echo "iot hub name: ${iothubname}\n"
-echo "location: ${location}\n"
-echo "funcappid: ${funcappid}\n"
-echo "storagename: ${storagename}\n"
-echo "containername: ${containername}\n"
+echo "iot hub name: ${iothubname}"
+echo "location: ${location}"
+echo "funcappid: ${funcappid}"
+echo "storagename: ${storagename}"
+echo "containername: ${containername}"
 
 echo "Installing azure cli extension..."
 az config set extension.use_dynamic_install=yes_without_prompt
@@ -23,4 +23,4 @@ git clone https://github.com/Azure-Samples/azuremaps-indoor-realtime-position-tr
 echo "Retrieving and uploading public files to blob storage..."
 az storage blob upload-batch --account-name $storagename -d $containername -s "./src/public"
 
-echo "Post deployment script completed!\n"
+echo "Post deployment script completed!"
