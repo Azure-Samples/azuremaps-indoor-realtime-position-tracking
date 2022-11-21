352ac8d10d5468e8a102c4625982989d6221d7b4
Oscar Naim <62676340+onaim-msft@users.noreply.github.com>
2022-11-21 14:41:37 -0800

    Script updates

--- deploy/postdeploy.sh
+++ deploy/postdeploy.sh
@@ -6,26 +6,23 @@ location=$3
 funcappid=$4
 storagename=$5
 containername=$6
+storagecs=$7
 
 echo "iot hub name: ${iothubname}"
 echo "location: ${location}"
 echo "funcappid: ${funcappid}"
 echo "storagename: ${storagename}"
 echo "containername: ${containername}"
+echo "storagecs: ${storagecs}"
 
 echo "Installing azure cli extension..."
-az config set extension.use_dynamic_install=yes_without_prompt > outputJSON
-echo "outputJSON="+$outputJSON
-
-az extension add --name azure-iot -y > outputJSON
-echo "outputJSON="+$outputJSON
+az config set extension.use_dynamic_install=yes_without_prompt
+az extension add --name azure-iot -y
 
 echo "Retrieving files..."
-git clone https://github.com/Azure-Samples/azuremaps-indoor-realtime-position-tracking.git > outputJSON
-echo "outputJSON="+$outputJSON
+git clone https://github.com/Azure-Samples/azuremaps-indoor-realtime-position-tracking.git
 
 echo "Retrieving and uploading public files to blob storage..."
-az storage blob upload-batch --account-name $storagename -d $containername -s "./src/public" > outputJSON
-echo "outputJSON="+$outputJSON
+az storage blob upload-batch --connection-string $storagecs --account-name $storagename -d $containername -s "./src/public"
 
 echo "Post deployment script completed!"
